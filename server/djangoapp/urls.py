78e26a84b1fb27455a25ddab02935a5c3e0f0dd1
SanjayKhatiChhetri <sanjaykhati579@gmail.com>
2024-08-18 15:59:32 -0400

    Build CarModel and CarMake Django Models: done

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,13 +7,16 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration' , view=views.registration, name='registration'),
+    path(route='register' , view=views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
     path(route='logout' , view=views.logout_request, name='logout'),
+
+    #get cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for add a review view
 
